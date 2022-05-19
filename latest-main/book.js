5c8301dab71efa6ecaa28b8ef3d48662aeb54f80
Mr-R-python <77397437+Mr-R-python@users.noreply.github.com>
2022-05-19 23:48:42 +0530

    Add files via upload

---
+++ latest-main/book.js
@@ -0,0 +1,85 @@
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCGzDEgwljxs92U-0HaWA67Ti0l52Rc4o4",
+  authDomain: "pet-priorities-galaxy.firebaseapp.com",
+  databaseURL: "https://pet-priorities-galaxy-default-rtdb.firebaseio.com",
+  projectId: "pet-priorities-galaxy",
+  storageBucket: "pet-priorities-galaxy.appspot.com",
+  messagingSenderId: "600114058410",
+  appId: "1:600114058410:web:dddea9a0ea9538e1c3acd3",
+  measurementId: "G-FHC9QYRKSW"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    
+    
+    
+    
+    
+    // Appointment section database System
+  
+  
+  var petwebDB = firebase.database().ref("book");
+  
+  document.getElementById("book").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+    e.preventDefault();
+    var service = getElementVal("service");
+    var pname = getElementVal("pet");
+    var name = getElementVal("name");
+    var number= getElementVal("number");
+    var email= getElementVal("email");
+    var date= getElementVal("date");
+    var time= getElementVal("time");
+    var description = getElementVal("description");
+  
+    saveMessages(name, service, pname, number, email, date, time, description);
+  
+    //   enable alert
+    document.querySelector(".alert").style.display = "block";
+  
+    //   remove the alert
+    setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+    }, 3000);
+  
+  //   reset the form
+  document.getElementById("book").reset();
+  }
+  
+  const saveMessages = (name,service, pname, number, email, date, time, description) => {
+    var newAppointment = petwebDB.push();
+  
+    newAppointment.set({
+      service:service,
+      pname: pname,
+      name: name,
+      number: number,
+      email: email,
+      date: date,
+      time: time,
+      description: description,
+    });
+  };
+    
+    const getElementVal = (id) => {
+      return document.getElementById(id).value;
+    };
+    
+  
+  
+  
+    
+    
+    
+  
