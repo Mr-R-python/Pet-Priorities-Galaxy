17d9b93ee1fb4433574637f53f6b163d8abe11d9
Mr-R-python <77397437+Mr-R-python@users.noreply.github.com>
2022-05-19 23:36:33 +0530

    Add files via upload

---
+++ hotal/hotal.js
@@ -0,0 +1,171 @@
+function HotelName(){
+    var name=document.getElementById("sweet").innerHTML
+    var nameL=document.getElementById("sweetl").innerHTML
+    var nameM=document.getElementById("sweetm").innerHTML
+    var nameE=document.getElementById("sweete").innerHTML
+    
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName1(){
+    var name=document.getElementById("marriot").innerHTML
+    var nameL=document.getElementById("marriotl").innerHTML
+    var nameM=document.getElementById("marriotm").innerHTML
+    var nameE=document.getElementById("marriote").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName2(){
+    var name=document.getElementById("honey").innerHTML
+    var nameL=document.getElementById("honeyl").innerHTML
+    var nameM=document.getElementById("honeym").innerHTML
+    var nameE=document.getElementById("honeye").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName3(){
+    var name=document.getElementById("rose").innerHTML
+    var nameL=document.getElementById("rosel").innerHTML
+    var nameM=document.getElementById("rosem").innerHTML
+    var nameE=document.getElementById("rosee").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName4(){
+    var name=document.getElementById("fun").innerHTML
+    var nameL=document.getElementById("funl").innerHTML
+    var nameM=document.getElementById("funm").innerHTML
+    var nameE=document.getElementById("fune").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName5(){
+    var name=document.getElementById("khushi").innerHTML
+    var nameL=document.getElementById("khushil").innerHTML
+    var nameM=document.getElementById("khushim").innerHTML
+    var nameE=document.getElementById("khushie").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName6(){
+    var name=document.getElementById("krishna").innerHTML
+    var nameL=document.getElementById("krishnal").innerHTML
+    var nameM=document.getElementById("krishnam").innerHTML
+    var nameE=document.getElementById("krishnae").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName7(){
+    var name=document.getElementById("aditya").innerHTML
+    var nameL=document.getElementById("adityal").innerHTML
+    var nameM=document.getElementById("adityam").innerHTML
+    var nameE=document.getElementById("adityae").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName8(){
+    var name=document.getElementById("shubhara").innerHTML
+    var nameL=document.getElementById("shubharal").innerHTML
+    var nameM=document.getElementById("shubharam").innerHTML
+    var nameE=document.getElementById("shubharae").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName9(){
+    var name=document.getElementById("varun").innerHTML
+    var nameL=document.getElementById("varunl").innerHTML
+    var nameM=document.getElementById("varunm").innerHTML
+    var nameE=document.getElementById("varune").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName10(){
+    var name=document.getElementById("tanishk").innerHTML
+    var nameL=document.getElementById("tanishkl").innerHTML
+    var nameM=document.getElementById("tanishkm").innerHTML
+    var nameE=document.getElementById("tanishke").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName11(){
+    var name=document.getElementById("dimpal").innerHTML
+    var nameL=document.getElementById("dimpall").innerHTML
+    var nameM=document.getElementById("dimpalm").innerHTML
+    var nameE=document.getElementById("dimpale").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName12(){
+    var name=document.getElementById("akhilg").innerHTML
+    var nameL=document.getElementById("akhill").innerHTML
+    var nameM=document.getElementById("akhilm").innerHTML
+    var nameE=document.getElementById("akhile").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName13(){
+    var name=document.getElementById("ramdev").innerHTML
+    var nameL=document.getElementById("ramdevl").innerHTML
+    var nameM=document.getElementById("ramdevm").innerHTML
+    var nameE=document.getElementById("ramdeve").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+function HotelName14(){
+    var name=document.getElementById("suresh").innerHTML
+    var nameL=document.getElementById("sureshl").innerHTML
+    var nameM=document.getElementById("sureshm").innerHTML
+    var nameE=document.getElementById("sureshe").innerHTML
+
+    localStorage.setItem("passValue",name);
+    localStorage.setItem("passValueL",nameL);
+    localStorage.setItem("passValueM",nameM);
+    localStorage.setItem("passValueE",nameE);
+}
+
+
+document.getElementById("HotelName").innerHTML = localStorage.getItem("passValue");
+document.getElementById("HotelNameL").innerHTML = localStorage.getItem("passValueL");
+document.getElementById("HotelNameM").innerHTML = localStorage.getItem("passValueM");
+document.getElementById("HotelNameE").innerHTML = localStorage.getItem("passValueE");
